a865d7412d0d52da0b971679597b5cc10304ad27
超级小星星 <62045849+cannnnnnnnnnnn@users.noreply.github.com>
2023-03-13 09:34:23 +0800

    初始化测试项目

---
+++ RTC_SHOW/cmake-build-debug/CMakeFiles/RTC_SHOW.elf.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Startup/startup_stm32f103zetx.s" "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/cmake-build-debug/CMakeFiles/RTC_SHOW.elf.dir/Core/Startup/startup_stm32f103zetx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xE"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Inc"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/gpio.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/main.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/main.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/stm32f1xx_it.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/syscalls.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/sysmem.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Core/Src/system_stm32f1xx.c" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "D:/Clion_STM32/CLionProjects/Slippery_UI/RTC_SHOW/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/RTC_SHOW.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
